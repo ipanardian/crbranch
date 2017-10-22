46b003edd2d1b6c4a36730363d189646394c7990
ipanardian <ipan.software@gmail.com>
2017-10-22 21:27:36 +0700

    Enhance main flow

--- crBranch.php
+++ crBranch.php
@@ -1,4 +1,7 @@
 <?php 
+
+namespace ipanardian\git;
+
 /**
  * crBranch
  * (c) 2016 Ipan Ardian
@@ -8,35 +11,124 @@
  * The MIT License
  */
 
-if (isset($_SERVER['REMOTE_ADDR'])) die('Permission denied.');
-error_reporting(E_ALL ^E_NOTICE);
-
-class crBranch
+class crBranch implements InterfaceCrBranch
 {
+	/**
+	 * arguments
+	 */
 	private $arg;
 	
+	/**
+	 * Constructor
+	 * @param array $argv
+	 */
 	function __construct($argv)
 	{
 		$this->arg = $argv ?: exit();
 	}
 
-	public function simplifyMyLife(callable $callback)
+	/**
+	 * Run
+	 * @return void
+	 */
+	public function run()
 	{
-		is_callable($callback) ?: exit();
-		$conf = $callback($this->arg) ?: exit('Empty return of callback');
-		$str = $this->_readline() ?: exit();
-		if (!empty($conf[1])) {
-			$str = $conf[1]. ' ' .$str;
+		switch ($this->arg) {
+
+			default:
+				$this->createBranch(function($arg) {
+					$baseBranch = 'development';
+					$prefix = '';
+					switch ($arg[1]) {
+						case '-f':
+							$prefix = 'feature';
+							break;
+				
+						case '-e':
+							$prefix = 'enhance';
+							break;
+				
+						case '-b':
+							$prefix = 'bugfix';
+							break;
+				
+						case '-h':
+							$prefix = 'hotfix';
+							$baseBranch = 'hotfix';
+							break;
+							
+						case '-hf':
+							$prefix = 'hotfeature';
+							break;
+						
+						case '-t':
+							$prefix = 'test';
+							break;
+				
+						case '-c':
+							$baseBranch = $arg[2] ?: exit('Missing argument base branch'. PHP_EOL);
+							break;
+						
+						default:
+							exit('Unknown parameter'. PHP_EOL);
+							break;
+					}
+				
+					return [$baseBranch, $prefix];
+				});
+			break;
 		}
-		$baseBranch = escapeshellcmd($conf[0]);
-		$newBranch = escapeshellcmd(preg_replace('/\s/', '_', trim(preg_replace('/(\W\s|\s\W)/', '', strtolower($str)))));
-		if (empty($newBranch)) exit('Empty new branch name');
-		passthru('git checkout '.$baseBranch);
-		passthru('git pull origin '.$baseBranch);
-		passthru('git checkout -b '.$newBranch);
 	}
 
-	protected function _readline()
+	/**
+	 * createBranchName
+	 * @param string $str
+	 * @return string $sanitizedBranchName
+	 */
+	public function createBranchName($str) 
+	{
+		return escapeshellcmd(preg_replace('/\s/', '_', trim(preg_replace('/(\W\s|\s\W)/', '', strtolower($str)))));
+	}
+
+	/**
+	 * execGitCommand
+	 * @param string $command
+	 * @return void
+	 */
+	public function execGitCommand($command) 
+	{
+		passthru("git $command");
+	}
+
+	/**
+	 * createBranch
+	 * @param callable $callback
+	 * @return void
+	 */
+	public function createBranch(callable $callback)
+	{
+		is_callable($callback) ?: exit();
+		$conf = $callback($this->arg) ?: exit('Empty return of callback'. PHP_EOL);
+		$str = $this->readline() ?: exit();
+
+		if (!empty($conf[1])) 
+			$str = $conf[1]. ' ' .$str;
+		
+		$baseBranch = escapeshellcmd($conf[0]);
+		$newBranch = $this->createBranchName($str);
+		if (empty($newBranch)) 
+			exit('Empty new branch name');
+
+		$this->execGitCommand('checkout '.$baseBranch);
+		$this->execGitCommand('pull origin '.$baseBranch);
+		$this->execGitCommand('checkout -b '.$newBranch);
+	}
+
+	/**
+	 * readline
+	 * @return string $line
+	 */
+	public function readline()
 	{
 		echo "Type: ";
 		try {
@@ -58,44 +150,3 @@ class crBranch
 		exit();
 	}
 }
-
-(new crBranch($argv))->simplifyMyLife(function($arg) {
-	$baseBranch = 'development';
-	$prefix = '';
-	switch ($arg[1]) {
-		case '-f':
-			$prefix = 'feature';
-			break;
-
-		case '-e':
-			$prefix = 'enhance';
-			break;
-
-		case '-b':
-			$prefix = 'bugfix';
-			break;
-
-		case '-h':
-			$prefix = 'hotfix';
-			$baseBranch = 'hotfix';
-			break;
-			
-		case '-hf':
-			$prefix = 'hotfeature';
-			break;
-		
-		case '-t':
-			$prefix = 'test';
-			break;
-
-		case '-c':
-			$baseBranch = $arg[2] ?: exit('Missing argument base branch');
-			break;
-		
-		default:
-			die('Unknown parameter');
-			break;
-	}
-
-	return [$baseBranch, $prefix];
-});
